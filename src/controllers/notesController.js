4defe292fed2d1b09fc586d70ab30869f9585d09
klytvinenko <58306149+klytvinenko@users.noreply.github.com>
2025-12-10 14:54:57 +0200

    new

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,8 +2,34 @@ import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+  const { page = 1, perPage = 10, tag, search} = req.query;
+  const skip = (page - 1) * perPage;
+
+  //const notes = await Note.find();
+  const notesQuery = Note.find();
+
+  if(search) {
+    notesQuery.where({$text : {$search : search}});
+  };
+
+  if(tag) {
+    notesQuery.where("tag").equals(tag);
+  }
+
+
+  const [totalNotes, notes] = await Promise.all([
+    notesQuery.clone().countDocuments(),
+    notesQuery.skip(skip).limit(perPage),
+  ]);
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalPages,
+    totalNotes,
+    notes,
+  });
 };
 
 export const getNoteById = async (req, res) => {
--- src/models/note.js
+++ src/models/note.js
@@ -20,4 +20,6 @@ const noteSchema = new Schema (
     },
     { timestamps: true }
 );
+
+noteSchema.index({title : "text", content: "text"});
 export const Note = model('Note', noteSchema);
