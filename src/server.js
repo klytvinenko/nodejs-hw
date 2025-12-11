584b16f404e256106895ace5d1d377fc0e279488
klytvinenko <58306149+klytvinenko@users.noreply.github.com>
2025-12-11 09:25:02 +0200

    change

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { logger } from "./middleware/logger.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
 import notesRoutes from "./routes/notesRoutes.js";
+import { errors } from "celebrate";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -21,7 +22,9 @@ app.use(cors());
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
- app.use(errorHandler);
+
+app.use(errors());
+app.use(errorHandler);
 
 await connectMongoDB();
 
