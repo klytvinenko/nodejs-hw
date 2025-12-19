3068928f054c8845ac8fa5d2051583d0bd0f9bd4
klytvinenko <58306149+klytvinenko@users.noreply.github.com>
2025-12-19 11:25:06 +0200

    change

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -31,10 +31,7 @@ export const getAllNotes = async (req, res) => {
 
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findById({
-    _id: noteId,
-    userId: req.user._id,
-  });
+  const note = await Note.findOne({ _id: noteId, userId: req.user._id });
 
   if (!note) {
     throw createHttpError(404, 'Note not found');
@@ -53,7 +50,7 @@ export const createNote = async (req, res) => {
 
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndDelete({_id: noteId, userId: req.user._id});
+  const note = await Note.findOneAndDelete({ _id: noteId, userId: req.user._id });
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
@@ -64,9 +61,7 @@ export const deleteNote = async (req, res) => {
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
 
-  const note = await Note.findByIdAndUpdate({_id: noteId, userId: req.user._id}, req.body, {
-    new: true,
-  });
+  const note = await Note.findOneAndUpdate({ _id: noteId, userId: req.user._id }, req.body, { new: true });
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
